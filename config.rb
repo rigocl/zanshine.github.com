af4fec32ec2e99fa1cbbf90516c6a7ddac373201
Stéphane Robert Richard <github@zanshine.com>
2011-10-07 21:32:17 +0200

    Added a post, minor tweaks

--- config.rb
+++ config.rb
@@ -5,5 +5,6 @@ css_dir = "css"
 sass_dir = "_lib/sass/src"
 images_dir = "images"
 javascripts_dir = "javascripts"
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
